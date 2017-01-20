6ce85bb0098347aaba0c9c2b5ecbd279d2b71fce
Gael Arrambide <gael_arrambide@hotmail.com>
2017-01-19 20:02:43 -0600

    adding quotes and picture

--- assets/js/education.js
+++ assets/js/education.js
@@ -6,13 +6,13 @@ var edu={
     degree: "The Coding Boot Camp at UT Austin",
     field: "Full Stack Flex Program",
     date: "Jul 2016 - Feb 2017",
-    awards: ""
+    visit: "https://www.codingbootcamputaustin.com"
   },{
     name: "Instituto Tecnológico de Nuevo Laredo",
     degree: "Mechatronic Engineer",
-    title: "",
+    field: "Advance Industrial Automation",
     date: "Aug 2010 - Dec 2014",
-    awards: "",
+    visit: "http://www.itnuevolaredo.edu.mx/index.php?option=com_content&view=article&id=57&Itemid=93",
   }
   ],
   displayIntro: function(){
@@ -22,9 +22,9 @@ var edu={
   displayEdu: function(){
     for (i=0; i<edu.his.length; i++){
       if (i%2===0){
-    $("#eduHistory").append("<div class='row'><div class='col s12 m6'><div class='card blue-grey darken-2'><div class='card-content white-text'><div class='col s2'><img src='assets/images/ut.jpg' alt='' class='circle responsive-img'></div><span class='card-title'>"+edu.his[i].name+"</span><ul style='font-style:italic; font-size:18px'>"+edu.his[i].degree+", "+edu.his[i].field+"</ul><ul style='font-style:italic;'>"+edu.his[i].date+"</ul><li>"+edu.his[i].awards+"</li></div><div class='card-action white-text'><p>"+"</p></div></div></div></div>");
+    $("#eduHistory").append("<div class='row'><div class='col s12 m6'><div class='card blue-grey darken-2'><div class='card-content white-text'><div class='col s3'><img src='assets/images/ut.jpg' alt='UT Logo' class='circle responsive-img'></div><span class='card-title'>"+edu.his[i].name+"</span><ul style='font-style:italic; font-size:18px'>"+edu.his[i].degree+", "+edu.his[i].field+"</ul><ul style='font-style:italic;'>"+edu.his[i].date+"</ul></div><div class='card-action white-text'><a href='"+edu.his[i].visit+"'>More Info</a></div></div></div></div>");
   } else {
-    $("#eduHistory").append("<div class='row'><div class='col s12 m6 right'><div class='card blue-grey darken-2'><div class='card-content white-text'><span class='card-title'>"+edu.his[i].name+"</span><ul style='font-style:italic; font-size:18px'>"+edu.his[i].degree+", "+edu.his[i].field+"</ul><ul style='font-style:italic;'>"+edu.his[i].date+"</ul><li>"+edu.his[i].awards+"</li></div><div class='card-action white-text'><p>"+"</p></div></div></div></div>");
+    $("#eduHistory").append("<div class='row'><div class='col s12 m6 right'><div class='card blue-grey darken-2'><div class='card-content white-text'><div class='col s3'><img src='assets/images/tec.jpg' alt='ITNL Logo' class='circle responsive-img'></div><span class='card-title'>"+edu.his[i].name+"</span><ul style='font-style:italic; font-size:18px'>"+edu.his[i].degree+", "+edu.his[i].field+"</ul><ul style='font-style:italic;'>"+edu.his[i].date+"</ul></div><div class='card-action white-text'><a href='"+edu.his[i].visit+"'>More Info</a></div></div></div></div>");
   }
   }
   },
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 var index={
   aboutMe1:"I’m a full stack developer apprentice born in Nuevo Laredo, Mexico now living a life changing experience at  Austin, TX. After 3 years working on retail jobs while studying and after finish my degree I realized that I was not where I want it to be so I decided to change the path I was going.",
   aboutMe2:"I may not have the experience working as a developer yet but I definitely have the passion that it needs to continue learning every single day. At school and my job I show determination on what I do, and work hard enough to accomplish what it needs to be done. Even though I consider myself to be an introvert I’m not afraid to be the leader of the team. Another strengths are time management, strategic mind and working under pressure.",
-  aboutMePic: "",
+  aboutMePic: "assets/images/me.jpg",
+  quote: ["If you're really honest with yourself about what you want out of life, life gives it to you.", "-Ted Mosby"],
+  quote2: ["Education never ends, Watson. It is a series of lessons, with the greatest for the last.", "-Arthur Conan Doyle"],
   iconInfo: [
     {
     icon: "code",
@@ -19,16 +21,24 @@ var index={
   displayAbout: function(){
     $("#aboutMe").append("<h4>About Me</h4>");
     $("#aboutMe").append("<div class='col s6'><h6 class='light' style='text-align:justify; text-indent:30px'>"+index.aboutMe1+"</h6><br><h6 class='light' style='text-align:justify; text-indent:30px'>"+index.aboutMe2+"</h6></div>");
-    $("#aboutMe").append("<div class='col s6><img class='responsive-img' src="+index.aboutMePic+"></div>'");
+    $("#aboutMe").append("<div class='col s6'><img src='"+index.aboutMePic+"'></div>");
   },
   displayIconSection: function(){
     for (i=0; i<index.iconInfo.length; i++){
     $("#iconSection").append("<div class='col s12 m4'><div class='icon-block'><h2 class='center blue-grey-text text-darken-2'><i class='material-icons'>"+index.iconInfo[i].icon+"</i></h2><h5 class='center'>"+index.iconInfo[i].title+"</h5><p class='light'>"+index.iconInfo[i].info+"</p></div></div>");
-  }
-  }
+    }
+  },
+  displayQuote:function(){
+    $("#quote").append("<h3 class='header center col s12 light'>'"+index.quote[0]+"'</h3><h4 class='right'>"+index.quote[1]+"</h4>");
+  },
+  displayQuote2: function(){
+      $("#quote2").append("<h3 class='header center col s12 light'>'"+index.quote2[0]+"'</h3><h4 class='right'>"+index.quote2[1]+"</h4>");
+  },
 };
 
 $(document).ready(function(){
   index.displayAbout();
   index.displayIconSection();
+  index.displayQuote();
+  index.displayQuote2();
 });
